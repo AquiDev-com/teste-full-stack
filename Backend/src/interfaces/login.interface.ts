7e201306fe9642a1e9e2e186cd65a812ed7ff585
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-09-25 13:11:35 -0300

    fix: user feedback in the token creation service.

--- Backend/src/interfaces/login.interface.ts
+++ Backend/src/interfaces/login.interface.ts
@@ -1,4 +1,9 @@
 import { z } from "zod";
 import { createLoginSchema } from "../schemas/login.schema";
+import { User } from "../entities";
 
 export type ILogin = z.infer<typeof createLoginSchema>;
+export interface ILoginResponse {
+  token: string;
+  user: User | {};
+}
--- Backend/src/services/login/createLogin.service.ts
+++ Backend/src/services/login/createLogin.service.ts
@@ -1,7 +1,7 @@
 import { compare } from "bcryptjs";
 import { AppDataSource } from "../../data-source";
 import { AppError } from "../../errors";
-import { ILogin } from "../../interfaces/login.interface";
+import { ILogin, ILoginResponse } from "../../interfaces/login.interface";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 import { IUserRepo } from "../../interfaces/users.interface";
@@ -9,7 +9,7 @@ import { User } from "../../entities";
 
 export const createLoginService = async (
   loginData: ILogin
-): Promise<string> => {
+): Promise<ILoginResponse> => {
   const userRepository: IUserRepo = AppDataSource.getRepository(User);
 
   const user: User | null = await userRepository.findOneBy({
@@ -37,5 +37,10 @@ export const createLoginService = async (
     }
   );
 
-  return token;
+  const { id, name, email, phone, birthday, gender } = user;
+
+  return {
+    token,
+    user: { id, name, email, phone, birthday, gender },
+  };
 };
